1c405f31d9738bf9c7e32148d90f8020d05580e4
Antonio Angotti <Angotti9@gmail.com>
2019-05-06 19:57:58 -0400

    Lab 11

---
+++ Lab11/Movie.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Movie
+    {
+        private string title;
+        private string category;
+
+        //public string Title
+        //{
+        //    set
+        //    {
+        //        title = value;
+        //    }
+        //    get
+        //    {
+        //        return title;
+        //    }
+        //}
+
+        //public string Category
+        //{
+        //    set
+        //    {
+        //        category = value;
+        //    }
+        //    get
+        //    {
+        //        return category;
+        //    }
+        //}
+
+        public Movie(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        public static void Menu(List<Movie> movies)
+        {
+
+            string response = "y";
+            while (response == "y")
+            {
+               
+                Console.WriteLine("\nThere are 10 movies in this list" + "\nWhat category are you interested in?");
+
+                switch (ReadUserInput())
+                {
+                    case "1":
+                        {
+                            PrintMovies(movies, "Animated");
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            PrintMovies(movies, "Drama");
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            PrintMovies(movies, "Horror");
+                            break;
+                        }
+
+                    case "4":
+                        {
+                            PrintMovies(movies, "SciFi");
+                            break;
+                        }
+                    case "5":
+                        response = "n";
+                        Console.WriteLine("\nGood bye!");
+
+                        break;
+
+                    default: break;
+                }
+                if (response != "y")
+                {
+                    response = "n";
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Would you like to continue: (y or n)");
+                    response = Console.ReadLine();
+                }
+
+            }
+        }
+        public static string ReadUserInput()
+        {
+            string userInput;
+            bool isUserInputValid = false;
+
+
+            do
+            {
+                Console.WriteLine("1.  Animated");
+                Console.WriteLine("2.  Drama");
+                Console.WriteLine("3.  Horror");
+                Console.WriteLine("4.  SciFi");
+                Console.WriteLine("5.  Quit");
+                userInput = Console.ReadLine();
+                userInput = userInput.Trim();
+                // if its not anything but 1-5 isUserInputValid will be false.
+                switch (userInput)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                        isUserInputValid = true;
+                        break;
+                    default:
+                        isUserInputValid = false;
+                        break;
+                }
+                if (!isUserInputValid)
+                {
+                    Console.Clear();
+                    Console.WriteLine("That's not valid input. Please input 1-5");
+                }
+            }
+            while (!isUserInputValid);
+
+            return userInput;
+
+        }
+        public static void PrintMovies(List<Movie> movies, string _category)
+        {
+            foreach (Movie film in movies)
+            {
+                if (film.category == _category)
+                {
+                    Console.WriteLine(film.title);  
+                }
+            }
+            Console.WriteLine("--------------------");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Lab11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Sort();
+
+            Movie movie1 = new Movie("The Lion King", "Animated");
+            movies.Add(movie1);
+
+            Movie movie2 = new Movie("The Godfather", "Drama");
+            movies.Add(movie2);
+
+            Movie movie3 = new Movie("The Conjuring", "Horror");
+            movies.Add(movie3);
+
+            Movie movie4 = new Movie("The Matrix", "SciFi");
+            movies.Add(movie4);
+
+            Movie movie5 = new Movie("Avengers: End Game", "SciFi");
+            movies.Add(movie5);
+
+            Movie movie6 = new Movie("The Dark Knight", "SciFi");
+            movies.Add(movie6);
+
+            Movie movie7 = new Movie("The Jungle Book", "Animated");
+            movies.Add(movie7);
+
+            Movie movie8 = new Movie("Avatar", "Animated");
+            movies.Add(movie8);
+
+            Movie movie9 = new Movie("Good Fellas", "Drama");
+            movies.Add(movie9);
+
+            Movie movie10 = new Movie("Scarface", "Drama");
+            movies.Add(movie10);
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+
+            Movie.Menu(movies);
+
+
+
+        }
+
+    }
+}
+
